ab55fe7c5655a0985d651333933412cd0de72b20
anilchejarla <anilchejarla205@gmail.com>
2017-08-08 15:48:16 +0530

    to jenkins....

---
+++ src/main/java/com/jenkinsgroup/App.java
@@ -0,0 +1,13 @@
+package com.jenkinsgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
